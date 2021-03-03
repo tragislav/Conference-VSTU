709908426aa3e547dbaab99619aeca1b4344ed04
Tragislav Work PC <s.dykomenko@gmail.com>
2021-03-03 09:42:53 +0200

    static change

--- src/static.js
+++ src/static.js
@@ -4,7 +4,7 @@ const MAIN_URL = "http://localhost:8080";
 // const MAIN_URL = "http://ictaiconf.vstu.by:8082";
 
 const AUTH_URL = "http://192.168.50.19:8081";
-// const AUTH_URL = "http://ictaiconf.vstu.by:8081/token";
+// const AUTH_URL = "http://ictaiconf.vstu.by:8081";
 
 // export default { EMPTY };
 // export default MAIN_URL;
