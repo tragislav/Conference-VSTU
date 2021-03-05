b82bc2307d1f6a3a8be6b6b66afd4e7fd9e64ba8
Tragislav Work PC <s.dykomenko@gmail.com>
2021-03-05 14:47:30 +0200

    Sorry for my stupid

--- src/static.js
+++ src/static.js
@@ -3,7 +3,7 @@ const EMPTY = "Empty Field";
 const MAIN_URL = "http://localhost:8080";
 // const MAIN_URL = "http://ictaiconf.vstu.by:8082";
 
-const AUTH_URL = "http://192.168.50.19:8081";
+const AUTH_URL = "http://localhost:8081";
 // const AUTH_URL = "http://ictaiconf.vstu.by:8081";
 
 // export default { EMPTY };
